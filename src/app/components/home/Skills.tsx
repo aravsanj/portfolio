1a1f7c82ad1e92f523e5697dc68a8561b04c3e42
Aravind Sanjeev <saravind436@gmail.com>
2023-05-30 15:24:39 +0530

    added skill anims

--- src/app/components/home/Skills.tsx
+++ src/app/components/home/Skills.tsx
@@ -11,31 +11,57 @@ import {
 import { GiSkills } from "react-icons/gi";
 import { BsGit } from "react-icons/bs";
 
-
-type Props = {}
+type Props = {};
 
 const Skills = (props: Props) => {
   return (
-    <section className="flex flex-col gap-y-10 sm:gap-y-24 justify-between items-center p-4 pt-8 sm:p-8 text-[#101820] bg-[#FDFD96]">
-    <div className="flex flex-col gap-y-10 sm:gap-y-12 justify-between items-center p-8 h-full">
-      <div className="flex flex-col items-center gap-y-2">
-        <GiSkills className="text-3xl" />
-        <h3 className="text-2xl sm:text-3xl">My skills</h3>
+    <section className="bg-skills flex flex-col gap-y-10 sm:gap-y-24 justify-between items-center p-4 pt-8 sm:p-8 text-[#101820] bg-[#FDFD96]">
+      <div className="flex flex-col gap-y-10 sm:gap-y-12 justify-between items-center p-8 h-full">
+        <div className="flex flex-col items-center gap-y-2">
+          <GiSkills className="text-3xl" />
+          <h3 className="text-2xl sm:text-3xl">My skills</h3>
+        </div>
+        <div className="grid grid-cols-3 grid-rows-2 gap-6 gap-x-10 text-[#101820] p-10 rounded-lg">
+          <a target="blank" href="https://react.dev/">
+            <FaReact className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a target="blank" href="https://www.typescriptlang.org/">
+            <SiTypescript className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a
+            target="blank"
+            href="https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"
+          >
+            <SiJavascript className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a
+            target="blank"
+            href="https://www.w3.org/standards/webdesign/htmlcss"
+          >
+            <SiCss3 className="text-2xl sm:text-3xl  grow" />
+          </a>
+          <a
+            target="blank"
+            href="https://www.w3.org/standards/webdesign/htmlcss"
+          >
+            <SiHtml5 className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a target="blank" href="https://git-scm.com/">
+            <BsGit className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a target="blank" href="https://www.figma.com/">
+            <SiFigma className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a target="blank" href="https://getbootstrap.com/">
+            <SiBootstrap className="text-2xl sm:text-3xl grow" />
+          </a>
+          <a target="blank" href="https://webflow.com/">
+            <SiWebflow className="text-2xl sm:text-3xl grow" />
+          </a>
+        </div>
       </div>
-      <div className="grid grid-cols-3 grid-rows-2 gap-6 gap-x-10 text-[#101820] p-10 rounded-lg">
-        <FaReact className="text-2xl sm:text-3xl" />
-        <SiTypescript className="text-2xl sm:text-3xl" />
-        <SiJavascript className="text-2xl sm:text-3xl" />
-        <SiCss3 className="text-2xl sm:text-3xl" />
-        <SiHtml5 className="text-2xl sm:text-3xl" />
-        <BsGit className="text-2xl sm:text-3xl" />
-        <SiFigma className="text-2xl sm:text-3xl" />
-        <SiBootstrap className="text-2xl sm:text-3xl" />
-        <SiWebflow className="text-2xl sm:text-3xl" />
-      </div>
-    </div>
-  </section>
-  )
-}
+    </section>
+  );
+};
 
-export default Skills
+export default Skills;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { GrCertificate, GrProjects, GrUserWorker } from "react-icons/gr";
-
-import Project from "./components/home/Utils/Project";
-import ListItem from "./components/home/Utils/ListItem";
-import Social from "./components/home/Utils/Social";
 import Intro from "./components/home/Intro";
 import Skills from "./components/home/Skills";
 import Projects from "./components/home/Projects";
